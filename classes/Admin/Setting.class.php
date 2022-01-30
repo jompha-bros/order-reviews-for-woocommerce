f7230fac2c49ab536f48369367f1d0f07809fe76
mashiur-dev <mr.shoyeb21@gmail.co>
2022-01-30 14:50:24 +0600

    review post added to ORFW menu

--- classes/Admin/Setting.class.php
+++ classes/Admin/Setting.class.php
@@ -65,15 +65,15 @@ class Setting
 				array($this, 'renderPage'),
 				0
 			),
-			array(
-				self::$pageSlug, 
-				'Order Reviews', 
-				'Order Reviews', 
-				'manage_woocommerce', 
-				'orfw-reviews', 
-				array($this, 'renderReviews'), 
-				1
-			),
+			// array(
+			// 	self::$pageSlug, 
+			// 	'Order Reviews', 
+			// 	'Order Reviews', 
+			// 	'manage_woocommerce', 
+			// 	'orfw-reviews', 
+			// 	array($this, 'renderReviews'), 
+			// 	1
+			// ),
 			array(
 				'woocommerce',
 				esc_html__( 'Order Reviews', 'order-reviews-for-woocommerce' ),
@@ -82,7 +82,7 @@ class Setting
 				'orfw-reviews-link',
 				function()
 				{
-					wp_safe_redirect( admin_url( 'admin.php?page=orfw-reviews' ), 301 ); 
+					wp_safe_redirect( admin_url( 'edit.php?post_type=orfw_review' ), 301 ); 
   					exit;
 				},
 				1
