9e014a2828697ea083f66541b5ebe67148f89816
rehanadil <rehanadil.dev@gmail.com>
2022-02-01 21:28:40 +0600

    Bug fix

--- classes/Front/ReviewInfo.class.php
+++ classes/Front/ReviewInfo.class.php
@@ -63,7 +63,7 @@ class ReviewInfo
 
         $key = array_search( intval($comment->comment_post_ID), $products );
 
-        if ( $key )
+        if ( $key !== false )
             unset( $products[ $key ] );
 
         $i  = 0;
@@ -82,7 +82,7 @@ class ReviewInfo
             if ( !$product )
                 continue;
 
-            $px[$i]  = array(
+            $px[]  = array(
                 'id'   => $product->get_id(),
                 'name' => $product->get_name(),
                 'url'  => $product->get_permalink(),
@@ -104,33 +104,33 @@ class ReviewInfo
             case 1:
                 echo sprintf(
                     esc_html__('This user also bought %s.', 'order-reviews-for-woocommerce'),
-                    '<a href="' . esc_url($px[1]['url']) . '" target="_blank">' . esc_html($px[1]['name']) . '</a>'
+                    '<a href="' . esc_url($px[0]['url']) . '" target="_blank">' . esc_html($px[0]['name']) . '</a>'
                 );
                 break;
             
             case 2:
                 echo sprintf(
                     esc_html__('This user also bought %s and %s.', 'order-reviews-for-woocommerce'),
-                    '<a href="' . esc_url($px[1]['url']) . '" target="_blank">' . esc_html($px[1]['name']) . '</a>',
-                    '<a href="' . esc_url($px[2]['url']) . '" target="_blank">' . esc_html($px[2]['name']) . '</a>'
+                    '<a href="' . esc_url($px[0]['url']) . '" target="_blank">' . esc_html($px[0]['name']) . '</a>',
+                    '<a href="' . esc_url($px[1]['url']) . '" target="_blank">' . esc_html($px[1]['name']) . '</a>'
                 );
                 break;
             
             case 3:
                 echo sprintf(
                     esc_html__('This user also bought %s, %s, and %s.', 'order-reviews-for-woocommerce'),
+                    '<a href="' . esc_url($px[0]['url']) . '" target="_blank">' . esc_html($px[0]['name']) . '</a>',
                     '<a href="' . esc_url($px[1]['url']) . '" target="_blank">' . esc_html($px[1]['name']) . '</a>',
-                    '<a href="' . esc_url($px[2]['url']) . '" target="_blank">' . esc_html($px[2]['name']) . '</a>',
-                    '<a href="' . esc_url($px[3]['url']) . '" target="_blank">' . esc_html($px[3]['name']) . '</a>'
+                    '<a href="' . esc_url($px[2]['url']) . '" target="_blank">' . esc_html($px[2]['name']) . '</a>'
                 );
                 break;
             
             default:
                 echo sprintf(
                     esc_html__('This user also bought %s, %s, %s and %s other items.', 'order-reviews-for-woocommerce'),
+                    '<a href="' . esc_url($px[0]['url']) . '" target="_blank">' . esc_html($px[0]['name']) . '</a>',
                     '<a href="' . esc_url($px[1]['url']) . '" target="_blank">' . esc_html($px[1]['name']) . '</a>',
                     '<a href="' . esc_url($px[2]['url']) . '" target="_blank">' . esc_html($px[2]['name']) . '</a>',
-                    '<a href="' . esc_url($px[3]['url']) . '" target="_blank">' . esc_html($px[3]['name']) . '</a>',
                     ($t - 3)
                 );
                 break;
